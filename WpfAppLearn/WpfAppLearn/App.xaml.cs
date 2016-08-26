be999ecf5bbaf0948c21d6c919f1a0e745766be5
Shivam Srivastava <shivam01990@gmail.com>
2016-08-26 17:29:51 +0530

    Login

--- WpfAppLearn/WpfAppLearn/App.xaml.cs
+++ WpfAppLearn/WpfAppLearn/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfAppLearn.Views;
 
 namespace WpfAppLearn
 {
@@ -13,5 +14,37 @@ namespace WpfAppLearn
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+           
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+             Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+             LogOn logon = new LogOn();
+            bool? res = logon.ShowDialog();
+            Application.Current.MainWindow = null;
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            //if (res.HasValue && res.Value && Authenticate(logon.UserName, logon.Password))
+            //{
+            //    Bootstrapper bs = new Bootstrapper();
+            //    bs.Run();
+            //}
+            //else
+            //{
+            //    MessageBox.Show(
+            //        "Application is exiting due to invalid credentials",
+            //        "Application Exit",
+            //        MessageBoxButton.OK,
+            //        MessageBoxImage.Error);
+            //    Application.Current.Shutdown(1);
+            //}
+
+
+           
+        }
     }
 }
---
+++ WpfAppLearn/WpfAppLearn/ViewModels/LogOnViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppLearn.ViewModels
+{
+    public class LogOnViewModel : BindableBase
+    {
+        private string resultMessage;
+        public string InteractionResultMessage
+        {
+            get
+            {
+                return this.resultMessage;
+            }
+            set
+            {
+                this.resultMessage = value;
+                this.OnPropertyChanged("InteractionResultMessage");
+            }
+        }
+        private string _username;
+        private string _password;
+
+        public string UserName
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                SetProperty(ref _username, value);
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                SetProperty(ref _password, value);
+            }
+        }
+
+        public LogOnViewModel()
+        {
+            this.LoginCommand = new DelegateCommand(this.Login);
+            this.NotificationRequest = new InteractionRequest<INotification>();
+        }
+
+        public ICommand LoginCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+        public InteractionRequest<INotification> NotificationRequest { get; private set; }
+
+        public void Login()
+        {
+            //this.NotificationRequest.Raise(
+            //   new Notification { Content = "Notification Message", Title = "Notification" },
+            //   n => { InteractionResultMessage = "The user was notified."; });
+
+
+        }
+
+    }
+}
